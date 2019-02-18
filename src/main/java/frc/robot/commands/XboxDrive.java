567ca31605a0a63446bb01f72c7b6d416725b476
zhangtravis <popcorn20020806@gmail.com>
2019-02-18 15:34:46 -0700

    Added XboxDrive

---
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+
+public class XboxDrive extends Command {
+
+  double kp = SmartDashboard.getNumber("kp", .025);
+	double max_thresh = .12;
+	double min_thresh = .08;
+  public XboxDrive() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.drive);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.gyro.reset();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(Math.abs(Robot.m_oi.getDriverRightX()) > max_thresh)
+    	{
+    		Robot.drive.setRightSpeed(-Robot.m_oi.getDriverRightX());
+        	Robot.drive.setLeftSpeed(Robot.m_oi.getDriverRightX());
+        	Robot.gyro.reset();
+    	}
+    	else if(Math.abs(Robot.m_oi.getDriverRightX()) > min_thresh)
+    	{
+    		Robot.drive.setRightSpeed(0);
+        	Robot.drive.setLeftSpeed(0);
+    	}
+    	
+		double angle = Robot.gyro.getAngle();
+    	//double angle = 0;
+		
+		Robot.drive.setRightSpeed((Robot.m_oi.getDriverLeftY() + angle*kp));
+    	Robot.drive.setLeftSpeed((Robot.m_oi.getDriverLeftY() - angle*kp));
+
+    	SmartDashboard.putNumber("Left Speed", (Robot.m_oi.getDriverLeftY() + angle*kp));
+    	SmartDashboard.putNumber("Right Speed", (Robot.m_oi.getDriverLeftY() - angle*kp));
+    	SmartDashboard.putNumber("Angle", Robot.gyro.getAngle());
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.drive.setRightSpeed(0);
+    Robot.drive.setLeftSpeed(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
